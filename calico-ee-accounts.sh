f0c2d8ff82b1af2a58a12bca0c986db8f947552a
Chris Man <chrisman@Chriss-MacBook-Pro.local>
2021-06-11 16:29:20 -0700

    Creating accounts and tokens for the UI

---
+++ calico-ee-accounts.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Install the following network policies to secure Calico Enterprise component communications
+kubectl create -f https://docs.tigera.io/manifests/tigera-policies.yaml
+
+echo "Enter new user to be created:"
+read username
+
+# create a service account in the default namespace
+kubectl create sa $username -n default
+
+# Give the service account permissions to access the Calico Enterprise Manager UI, and a Calico Enterprise cluster role
+kubectl create clusterrolebinding $username-access --clusterrole tigera-network-admin --serviceaccount default:$username
+
+# Obtain token for service account which is used to log into the UI
+kubectl get secret $(kubectl get serviceaccount $username -o jsonpath='{range .secrets[*]}{.name}{"\n"}{end}' | grep token) -o go-template='{{.data.token | base64decode}}' && echo > ui-token.txt
+
+# Obtain the password for user elastic to log into Kibana
+kubectl -n tigera-elasticsearch get secret tigera-secure-es-elastic-user -o go-template='{{.data.elastic | base64decode}}' && echo > kibana-token.txt
